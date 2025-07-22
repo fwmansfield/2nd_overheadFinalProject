ea1d441b2f2041015e5b905c799854e9e49884da
Fred Mansfield <fredgybrah@gmail.com>
2025-07-22 10:19:54 -0400

    Login capability is now available

--- front-end/src/NewsReader.js
+++ front-end/src/NewsReader.js
@@ -3,12 +3,16 @@ import { Articles } from "./Articles";
 import { useState, useEffect } from "react";
 import { exampleQuery, exampleData } from "./data";
 import { SavedQueries } from "./SavedQueries";
+import { LoginForm } from "./LoginForm";
 
 export function NewsReader() {
   const [query, setQuery] = useState(exampleQuery); // latest query send to newsapi
   const [data, setData] = useState(exampleData); // current data returned from newsapi
+  const [currentUser, setCurrentUser] = useState(null);
+  const [credentials, setCredentials] = useState({ user: "", password: "" });
   const urlNews = "/news";
   const urlQueries = "/queries";
+  const urlUsersAuth = "/users/authenticate";
   const [savedQueries, setSavedQueries] = useState([{ ...exampleQuery }]);
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
 
@@ -19,6 +23,37 @@ export function NewsReader() {
   useEffect(() => {
     getQueryList();
   }, []);
+
+  async function login() {
+    if (currentUser !== null) {
+      // logout
+      setCurrentUser(null);
+    } else {
+      // login
+      try {
+        const response = await fetch(urlUsersAuth, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(credentials),
+        });
+        if (response.status === 200) {
+          setCurrentUser({ ...credentials });
+          setCredentials({ user: "", password: "" });
+        } else {
+          alert(
+            "Error during authentication! " +
+              credentials.user +
+              "/" +
+              credentials.password
+          );
+          setCurrentUser(null);
+        }
+      } catch (error) {
+        console.error("Error authenticating user:", error);
+        setCurrentUser(null);
+      }
+    }
+  }
 
   async function getQueryList() {
     try {
@@ -91,6 +126,13 @@ export function NewsReader() {
 
   return (
     <div>
+      {" "}
+      <LoginForm
+        login={login}
+        credentials={credentials}
+        currentUser={currentUser}
+        setCredentials={setCredentials}
+      />
       <div>
         <section className="parent">
           <div className="box">
