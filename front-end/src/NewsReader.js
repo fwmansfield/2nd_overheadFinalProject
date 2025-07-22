6f4c4eeabac217b7d148148559a3f2565734d655
Fred Mansfield <fredgybrah@gmail.com>
2025-07-22 09:54:57 -0400

    now gets live article list from server

--- front-end/src/NewsReader.js
+++ front-end/src/NewsReader.js
@@ -1,17 +1,17 @@
-
-import { QueryForm } from './QueryForm';
-import { Articles } from './Articles';
-import { useState, useEffect } from 'react';
-import { exampleQuery ,exampleData } from './data';
+import { QueryForm } from "./QueryForm";
+import { Articles } from "./Articles";
+import { useState, useEffect } from "react";
+import { exampleQuery, exampleData } from "./data";
 
 export function NewsReader() {
   const [query, setQuery] = useState(exampleQuery); // latest query send to newsapi
-  const [data, setData] = useState(exampleData);   // current data returned from newsapi
+  const [data, setData] = useState(exampleData); // current data returned from newsapi
+  const urlNews = "/news";
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
 
   useEffect(() => {
     getNews(query);
-  }, [query])
+  }, [query]);
 
   function onFormSubmit(queryObject) {
     setQuery(queryObject);
@@ -19,7 +19,21 @@ export function NewsReader() {
 
   async function getNews(queryObject) {
     if (queryObject.q) {
-        setData(exampleData);
+      try {
+        const response = await fetch(urlNews, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(queryObject),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
     } else {
       setData({});
     }
@@ -27,21 +41,22 @@ export function NewsReader() {
 
   return (
     <div>
-      <div >
-        <section className="parent" >
+      <div>
+        <section className="parent">
           <div className="box">
-            <span className='title'>Query Form</span>
+            <span className="title">Query Form</span>
             <QueryForm
               setFormObject={setQueryFormObject}
               formObject={queryFormObject}
-              submitToParent={onFormSubmit} />
+              submitToParent={onFormSubmit}
+            />
           </div>
           <div className="box">
-            <span className='title'>Articles List</span>
+            <span className="title">Articles List</span>
             <Articles query={query} data={data} />
           </div>
         </section>
       </div>
     </div>
-  )
+  );
 }
